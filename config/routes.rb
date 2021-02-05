06f50ed2b6198e1d92a180c42cb03f0c9f99edb1
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-02-05 04:54:41 +0000

    Add public devise controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   root to: 'public/homes#top'
+
+
+  devise_for :members, skip: 'registrations', controllers: {
+  sessions: 'public/sessions'
+  }
+
+  devise_scope :member do
+    get 'edit' => 'public/registrations#edit'
+    get '/members/sign_up' => 'public/registrations#new', as: :new_member_registration
+    post '/members' => 'public/registrations#create'
+  end
+
+
 end
