9a18b98751a8c37708e5b0ac3af42b4b1bf5e655
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-02-06 06:45:43 +0000

    Add postcomment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_034815) do
+ActiveRecord::Schema.define(version: 2021_02_06_064322) do
 
   create_table "actions", force: :cascade do |t|
     t.integer "member_id"
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 2021_02_06_034815) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "post_id"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
