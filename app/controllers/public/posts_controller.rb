843b519daf5cc66257c50c34f56eba4871195249
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-02-06 06:20:05 +0000

    Finish basic create/edit/update/destroy action [public/posts]

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -3,18 +3,25 @@ class Public::PostsController < ApplicationController
  def create
     @post = Post.new(post_params)
     @post.member_id = current_member.id
-    #binding.pry
     @post.save
     redirect_to request.referer
  end
 
   def edit
+    @post = Post.find(params[:id])
+    @genres = Genre.all
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to member_path
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to request.referer
   end
 
   private
