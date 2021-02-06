531bb58bb96bee1b10bab801020085c8d9ec519b
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-02-06 08:29:49 +0000

    Add public/postcomments/show destroy action

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -3,24 +3,21 @@ class Public::PostCommentsController < ApplicationController
   def show
    @post = Post.find(params[:id])
    @post_comment = PostComment.new
+   #コメント一覧表示用インスタンス
    @post_comments = PostComment.where(post_id: params[:id])
   end
 
   def create
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.member_id = current_member.id
-    #binding.pry
     @post_comment.save
     redirect_to request.referer
   end
 
-  def edit
-  end
-
-  def update
-  end
-
   def destroy
+    @post_comment = PostComment.find(params[:id])
+    @post_comment.destroy
+    redirect_to request.referer
   end
 
   private
