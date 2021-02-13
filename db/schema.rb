20e9d048b672b9f68f5109062316a6750b62db06
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-02-13 07:23:39 +0000

    Add inquiry model and mailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_12_083016) do
+ActiveRecord::Schema.define(version: 2021_02_13_072148) do
 
   create_table "actions", force: :cascade do |t|
     t.integer "member_id"
@@ -74,6 +74,13 @@ ActiveRecord::Schema.define(version: 2021_02_12_083016) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string "name"
+    t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
