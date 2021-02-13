0bd66198fc1cf35a088898f88822b5b5a603a5a8
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-02-13 07:35:34 +0000

    Finish inquiry mail setting

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,2 +1,12 @@
 class InquiryMailer < ApplicationMailer
+
+  def send_mail(inquiry)
+    @inquiry = inquiry
+    mail(
+      from: 'system@example.com',
+      to: ENV['SECRET_ADDRESS'],
+      subject: 'お問い合わせ通知'
+    )
+  end
+
 end
