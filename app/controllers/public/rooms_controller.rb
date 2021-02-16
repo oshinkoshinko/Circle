5a6a7245a9d9bf2ce00a22cec514326dc5701dbf
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-02-16 05:01:22 +0000

    Update public/rooms/index to show chat-message-list and add link public/chats/_messages

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -4,16 +4,19 @@ class Public::RoomsController < ApplicationController
   def index
     #ログインユーザが存在するchat_roomを取得
     @chat_rooms = ChatRoom.where(member_id: current_member.id)
-    @all_rooms = ChatRoom.where
-    #@chats = @chat_rooms.room.chats
-    #ログインユーザが持つroomを取得
-    @rooms = @chat_rooms.pluck(:room_id)
-    #ログインユーザが存在するroomの中のメッセージを取得
-    @room_chats = Chat.where(room_id: @rooms).order("created_at DESC")
+    # @all_rooms = ChatRoom.where
+    # #@chats = @chat_rooms.room.chats
+    # #ログインユーザが持つroomを取得
+    # @rooms = @chat_rooms.pluck(:room_id)
+    # #ログインユーザが存在するroomの中のメッセージを取得
+    # @room_chats = Chat.where(room_id: @rooms).order("created_at DESC")
 
-    @room_members = ChatRoom.where(room_id: @rooms)
-    #binding.pry
-    #@rooms = Room.where(id: @chats.room_id)
+    # @room_members = ChatRoom.where(room_id: @rooms)
+    # #binding.pry
+    # #@rooms = Room.where(id: @chats.room_id)
+
+    @rooms = current_member.rooms
+
   end
 
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ApplicationRecord
   has_many :chat_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
+  has_many :members, through: :chat_rooms
 end
