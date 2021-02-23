ed868d794d9538735349e65697dc2cafb225fac9
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-02-23 04:44:16 +0000

    Add anchor-scroll by js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -149,3 +149,17 @@ document.addEventListener("turbolinks:load", function() {
     }
   });
 })
+
+//投稿一覧ページアンカースクロール
+document.addEventListener("turbolinks:load", function() {
+  $(function () {
+    $('a[href^="#"]').click(function () {
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, 'swing');
+        return false;
+    });
+  });
+})
