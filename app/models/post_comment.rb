eff8c900169192b794f4908cdd155ca8943fa0c7
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-02-26 09:23:13 +0000

    Add length validation to post and postcomment and feedback and event-content

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -3,6 +3,6 @@ class PostComment < ApplicationRecord
   belongs_to :post
   has_many :notifications, dependent: :destroy
 
-  validates :comment, presence: true
+  validates :comment, presence: true, length: { maximum: 140 }
 
 end
