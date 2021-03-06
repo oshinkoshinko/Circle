2cb95f8b15713ea54d4613507284866fb553e4ba
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-03-06 11:52:34 +0000

    Solve n+1 admin/events controller

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -2,7 +2,7 @@ class Admin::EventsController < ApplicationController
   before_action :authenticate_admin!,except: [:top]
 
   def index
-    @events = Event.all.order("started_at ASC")
+    @events = Event.all.order("started_at ASC").includes(:member, :genre, :event_members)
   end
 
 end
