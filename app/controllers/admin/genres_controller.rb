5e14b0cbae0f7989ab07907aecc222eba42212dd
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-03-06 06:35:46 +0000

    Add genre edit update action

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -30,6 +30,19 @@ class Admin::GenresController < ApplicationController
     end
   end
 
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def genre_params
